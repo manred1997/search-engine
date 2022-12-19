c594bc13149f00787bb247656a3a9d4add38a33b
Tran Hoang Vu <manred1997@Trans-MacBook-Pro.local>
2022-12-19 16:27:55 +0700

    [Add] Binary Search

---
+++ services/auto-complete/src/retrieval/binarysearch.py
@@ -0,0 +1,13 @@
+from bisect import bisect_left
+
+# Check if it contains suffix
+def bisect_contains_check(hist_list, prefix):
+    try:
+        return hist_list[bisect_left(hist_list, prefix)].startswith(prefix)
+    except IndexError:
+        return False
+
+# Returns the prefix keys
+def bisect_list_slice(hist_list, prefix):
+    return hist_list[bisect_left(hist_list, prefix):
+         bisect_left(hist_list, prefix[:-1] + chr(ord(prefix[-1])+1))]
