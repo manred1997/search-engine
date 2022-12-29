65181ec046d71cd881b71e2c59ed344bd6fae0be
Tran Hoang Vu <manred1997@Trans-MacBook-Pro.local>
2022-12-29 16:48:57 +0700

    [Update] spelling error component

--- services/auto-correction/src/spelling_error/component.py
+++ services/auto-correction/src/spelling_error/component.py
@@ -10,6 +10,7 @@ from src.utils.constants import (
     ALPHABET,
     ALPHABET_LOWERCASE,
     ALPHABET_UPPERCASE,
+    ALPHABET_VN,
     DIGITS,
     PUNCTUATION,
 )
@@ -64,3 +65,35 @@ def get_possible_word_from_vni_error(word):
                 possible_words += VNI_ERROR[char][1]
     
     return [possible_words]
+
+
+def get_possible_word_from_edit_error(word):
+    """Create all possible words from edit error
+    :param word
+    :return possible words list from edit error
+    """
+    if len(word) == 1:
+         return [word]
+
+    type_edit = random.choices(['delete', 'insert', 'permute', 'replace'], weights=(35, 17, 39, 9), k=1)[0]
+
+    position_error = random.randint(0, len(word)-1)
+
+    if type_edit == 'delete':  # Delete Error
+        possible_words = [word[:position_error] + word[position_error+1:]]
+    elif type_edit == 'insert':    # Insert Error
+        possible_words = [word[:position_error] + random.choice(ALPHABET_VN) + word[position_error:]]
+    elif type_edit == 'permute': # Permute Error
+        if position_error == 0:
+            possible_words = [word[position_error+1] + word[position_error] + word[position_error+2:]]
+        elif position_error == len(word) - 1:
+            possible_words = [word[:position_error-1] + word[position_error] + word[position_error-1]]
+        else:
+            if random.gauss(0, 1) < 0:
+                possible_words = [word[:position_error-1] + word[position_error] + word[position_error-1] + word[position_error+1:]]
+            else:
+                possible_words = [word[:position_error] + word[position_error+1] + word[position_error] + word[position_error+2:]]
+    else: # Replace Error/ Fat-Finger Error
+        possible_words = random.choice(get_possible_word_from_fat_finger_error(word)[:-1])
+
+    return possible_words
